7646074c192fb923800b246348cde39b753840b0
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2022-05-06 17:13:10 +0900

    hinawa: code refactoring for top level module
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- hinawa/src/lib.rs
+++ hinawa/src/lib.rs
@@ -1,43 +1,26 @@
+// SPDX-License-Identifier: MIT
 #[macro_use]
 extern crate glib;
 extern crate glib_sys;
 extern crate gobject_sys;
-extern crate libc;
 extern crate hinawa_sys;
-
-pub use glib::Error;
+extern crate libc;
 
 mod auto;
-pub use auto::*;
-
-mod fw_node;
-pub use fw_node::*;
-
-mod fw_req;
-pub use fw_req::*;
-
-mod fw_resp;
-pub use fw_resp::*;
-
-mod fw_fcp;
-pub use fw_fcp::*;
-
-mod snd_unit;
-pub use snd_unit::*;
-
-mod snd_efw;
-pub use snd_efw::*;
-
-mod snd_tscm;
-pub use snd_tscm::*;
-
 mod enums;
-pub use enums::*;
-
+mod fw_fcp;
+mod fw_node;
+mod fw_req;
+mod fw_resp;
+mod snd_efw;
 mod snd_motu;
-pub use snd_motu::*;
-
 mod snd_motu_register_dsp_parameter;
-pub use snd_motu_register_dsp_parameter::*;
+mod snd_tscm;
+mod snd_unit;
 
 pub mod subclass;
+
+pub use {
+    auto::*, enums::*, fw_fcp::*, fw_node::*, fw_node::*, fw_req::*, fw_resp::*, snd_efw::*,
+    snd_motu::*, snd_motu_register_dsp_parameter::*, snd_tscm::*, snd_unit::*,
+};
